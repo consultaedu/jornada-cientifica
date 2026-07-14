6cd70a0e5cb19627882fe1634f46da29d81b5273
uniconsulta <marcosdalleprane2@gmail.com>
2026-07-14 01:01:06 -0300

    sdfs

--- config.js
+++ config.js
@@ -9,8 +9,8 @@ const CONFIG_EVENTO = {
   periodo: "10 a 13 de novembro",
 
   datasEvento: {
-    inicio: "2026-07-13T22:00:00-03:00",
-    fim: "2026-07-15T01:00:00-03:00"
+    inicio: "2026-07-12T22:00:00-03:00",
+    fim: "2026-07-13T01:00:00-03:00"
   },
 
   links: {
